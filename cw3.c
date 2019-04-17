55ef0805d8d64a0752356fdac2253a8fe15cd8bf
INPGArtur <49718266+INPGArtur@users.noreply.github.com>
2019-04-17 13:58:48 +0200

    Create cw3.c

---
+++ cw3.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+int tabmax(int tab[], int n);
+
+int main()
+{
+    int tab[5] = {1,2,3,5,4};
+    int max;
+    
+    
+    max = tabmax(tab, 5);
+    printf("Najwieksza wartosc to: %d\n", max);
+    
+    return 0;
+}
+
+int tabmax(int tab[], int n)
+{
+    int i, max;
+    max = tab[0];
+    for(i=1; i<n; i++)
+    {
+        if(tab[i] > max)
+            max = tab[i];
+    }
+    
+    
+    return max;
+}
